c36b6d9a9669acaf714a31bc4fbfa01295a2f7b6
MarinaSpadetto <ninahspadetto@hotmail.com>
2023-09-22 14:12:10 -0300

    feat: alter field plataform_fee

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from property.models import Property
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Advertisement(models.Model):
 
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     platform_name = models.CharField(max_length=255)
-    platform_fee = models.DecimalField(max_digits=10, decimal_places=2)
+    platform_fee = models.DecimalField(max_digits=10, decimal_places=2, validators=[
+                                       MinValueValidator(0), MaxValueValidator(100)])
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
