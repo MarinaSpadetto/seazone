bcafeb6985953bbb68ede87dd6262277c11e875a
MarinaSpadetto <ninahspadetto@hotmail.com>
2023-09-22 14:32:58 -0300

    feat: add authentication API

--- advertisement/api/views.py
+++ advertisement/api/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from advertisement.models import Advertisement
 from advertisement.api.serializer import AdvertisementSerializer
-from rest_framework.response import Response
 
 
 class AdvertisementViewSet(viewsets.ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     http_method_names = ['get', 'post', 'put', 'patch']
+    permission_classes = [IsAuthenticated]
